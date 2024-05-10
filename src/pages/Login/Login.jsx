050799796dd242000435665478745b51a21ba388
Akbar <akbarjonjorayev001@gmail.com>
2024-05-10 19:51:07 +0500

    check inputs' value

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,9 @@
 import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Input from '../../components/Input';
+import { isValidUsername } from '../../js/utils/checker';
+import { addError } from '../../js/utils/errorInput';
 import './Login.css';
 
 export default function Login() {
@@ -7,9 +11,25 @@ export default function Login() {
   const passwordInput = useRef();
 
   function login() {
+    const usernameVal = usernameInput.current.value;
+    const [username, companyName] = usernameVal.split('@');
+
+    if (!isValidUsername(username) || !isValidUsername(companyName)) {
+      toast.error(`Foydalanuvchi nomi noto'g'ri`);
+      addError(usernameInput.current);
+      return;
+    }
+
+    const passwordVal = passwordInput.current.value;
+    if (!passwordVal) {
+      toast.error('Parol kiriting');
+      addError(passwordInput.current);
+      return;
+    }
+
     const loginData = {
-      username: usernameInput.current.value,
-      password: passwordInput.current.value,
+      username: usernameVal,
+      password: passwordVal,
     };
 
     console.log(loginData);
@@ -17,6 +37,18 @@ export default function Login() {
 
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className="login_page center_con">
         <div className="login_con">
           <h2>Log in</h2>
