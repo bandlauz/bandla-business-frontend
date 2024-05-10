c6c4d487c9a0b40522f72f882b3e65319f18ab76
Akbar <akbarjonjorayev001@gmail.com>
2024-05-10 16:44:47 +0500

    Navbar deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import './css/App.css';
 
 export default function App() {
   return (
     <>
-      <Navbar />
       <div className="content-body">Bandla Business Log in</div>
       <Footer />
     </>
