12d7708f11923025abf7d4f6bf0bf2184de5f03a
Akbar <akbarjonjorayev001@gmail.com>
2024-05-10 16:26:09 +0500

    new -> Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import './css/Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <Divider />
+      <Box p={1}>
+        <Container>
+          <Grid
+            container
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Grid item xs={6} align="left" style={{ whiteSpace: 'nowrap' }}>
+              <Typography variant="body2" color="text.secondary">
+                &copy;
+                <Link color="inherit" href="/">
+                  Bandla
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'. Barcha huquqlar himoyalangan'}
+              </Typography>
+            </Grid>
+            <Grid item xs={6} align="right">
+              <Link href="https://www.instagram.com/bandlauz" color="inherit">
+                <i className="fa-brands fa-instagram fa-2x"></i>
+              </Link>{' '}
+              <Link href="https://t.me/bandlauz" color="inherit">
+                <i className="fa-brands fa-telegram fa-2x"></i>
+              </Link>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+}
+
+export default Footer;
